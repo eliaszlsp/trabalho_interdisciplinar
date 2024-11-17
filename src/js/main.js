e1b2003ece4dbcf96b60a19c8099100a34f2b2c9
eliaszlsp <estudoselias@hotmail.com>
2024-11-17 03:32:09 -0300

    add:html, responsidade,main

--- src/js/main.js
+++ src/js/main.js
@@ -5,37 +5,53 @@ const targetDiv = document.querySelector(".nav");
 const image = document.querySelector(".logo-type");
 const logoNav = document.querySelector(".logo");
 const linksNav = document.querySelectorAll(".navbar-menu li a");
-console.log(linksNav);
+const menuHamburger = document.querySelector("#menuHamburguer");
+const menuHamburgerLinha = document.querySelectorAll(".linha");
+const hero = document.querySelector("#hero");
 // Escuta o evento de scroll
 window.addEventListener("scroll", () => {
   const scrollTop = window.scrollY || document.documentElement.scrollTop;
 
-  if (scrollTop >= 1035) {
-    if (scrollTop > 0 && scrollTop > lastScrollTop) {
-      // Usuário está rolando para baixo
-      if (!targetDiv.contains(image)) {
-        // targetDiv.insertBefore(image, targetDiv.firstChild); // Move a imagem para o targetDiv
-        logoNav.classList.add("scroll");
-        logoNav.style.display = "block";
-        linksNav.forEach((element) => {
-          element.style.color = "#333";
-          element.style.textShadow = "0 0 0";
-        });
-      }
+  if (window.innerWidth < 884) {
+    const finalDoElento = hero.getBoundingClientRect().bottom;
+    if (finalDoElento < 22 && !menuHamburger.classList.contains("ativo")) {
+      console.log(finalDoElento);
+      menuHamburgerLinha.forEach((element) => {
+        element.style.backgroundColor = "black";
+      });
+    } else {
+      menuHamburgerLinha.forEach((element) => {
+        element.style.backgroundColor = "white";
+      });
     }
-    header.classList.add("scroll-header");
-  } else if (scrollTop < 1035) {
-    header.classList.remove("scroll-header");
-    logoNav.style.display = "none";
-    linksNav.forEach((element) => {
-      element.style.color = "#f3f0f0";
-      element.style.textShadow = "0px 0px 10px rgb(0, 0, 0)";
-    });
-    if (scrollTop === 0) {
-      // Usuário chegou no topo da página
-      if (!sourceDiv.contains(image)) {
-        // sourceDiv.insertBefore(image, sourceDiv.firstChild); // Move a imagem de volta para o sourceDiv
-        logoNav.classList.remove("scroll");
+  } else {
+    if (scrollTop >= 1035) {
+      if (scrollTop > 0 && scrollTop > lastScrollTop) {
+        // Usuário está rolando para baixo
+        if (!targetDiv.contains(image)) {
+          // targetDiv.insertBefore(image, targetDiv.firstChild); // Move a imagem para o targetDiv
+          logoNav.classList.add("scroll");
+          logoNav.style.display = "block";
+          linksNav.forEach((element) => {
+            element.style.color = "#333";
+            element.style.textShadow = "0 0 0";
+          });
+        }
+      }
+      header.classList.add("scroll-header");
+    } else if (scrollTop < 1035) {
+      header.classList.remove("scroll-header");
+      logoNav.style.display = "none";
+      linksNav.forEach((element) => {
+        element.style.color = "#f3f0f0";
+        element.style.textShadow = "0px 0px 10px rgb(0, 0, 0)";
+      });
+      if (scrollTop === 0) {
+        // Usuário chegou no topo da página
+        if (!sourceDiv.contains(image)) {
+          // sourceDiv.insertBefore(image, sourceDiv.firstChild); // Move a imagem de volta para o sourceDiv
+          logoNav.classList.remove("scroll");
+        }
       }
     }
   }
@@ -280,4 +296,9 @@ nossaCausa.forEach((icon, index) => {
       if (el !== icon) el.classList.add("inactive");
     });
   });
+});
+
+menuHamburger.addEventListener("click", function () {
+  this.classList.toggle("ativo");
+  document.querySelector(".hamburger-nav").classList.toggle("ativo");
 });
