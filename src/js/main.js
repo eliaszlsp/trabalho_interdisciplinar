a2bf8c7db3a6542042199745b1273ea824ae0781
Caio Morelli <caiomorelli.it@gmail.com>
2024-11-17 13:11:15 -0300

    Alterado os textos de algumas partes do site que são geradas dinamicamente para ter os conteudos da pagina corretos

--- src/js/main.js
+++ src/js/main.js
@@ -64,25 +64,25 @@ window.addEventListener("scroll", () => {
 const cards = document.querySelector(".produtos-wrapper");
 const infosCards = [
   {
-    imagem: "src/imagens/cards/_DSC0180.JPG",
+    imagem: "src/imagens/cards/Bolsas-juntas.JPEG",
     nome: "Produto 1",
-    title: "Produto Sustentável 1",
-    descricao: "Descrição breve do produto eco-friendly...",
+    title: "Bolsas Cosmica",
+    descricao: "A linha de bolsas da Cósmica Upcycle é um verdadeiro...",
   },
   {
     imagem: "src/imagens/cards/127 - CHEILAWIGGERS - 849A0239.jpg",
     nome: "Produto 2",
-    title: "Produto Sustentável 2",
-    descricao: "Descrição breve do produto eco-friendly...",
+    title: "Mochila",
+    descricao: "Upeksha significa equanimidade. Mochila ideal para...",
   },
   {
     imagem: "src/imagens/cards/IMG_1286 (1).jpg",
     nome: "Produto 3",
-    title: "Produto Sustentável 3",
-    descricao: "Descrição breve do produto eco-friendly...",
+    title: "Basic bag",
+    descricao: "A Basic bag é a bolsa ideal para quem gosta de sair...",
   },
   {
-    imagem: "src/imagens/cards/IMG_3202.jpg",
+    imagem: "src/imagens/cards/Mochila.JPEG",
     nome: "Produto 4",
     title: "Produto Sustentável 4",
     descricao: "Descrição breve do produto eco-friendly...",
@@ -90,14 +90,14 @@ const infosCards = [
   {
     imagem: "src/imagens/cards/IMG_5863.jpg",
     nome: "Produto 5",
-    title: "Produto Sustentável 5",
-    descricao: "Descrição breve do produto eco-friendly...",
+    title: "Pochetes",
+    descricao: "A pochete Luna é a bolsa ideal para quem gosta de sair...",
   },
   {
     imagem: "src/imagens/cards/Upcycle afetivo.png",
     nome: "Produto 6",
-    title: "Produto Sustentável 6",
-    descricao: "Descrição breve do produto eco-friendly...",
+    title: "Upcycle Afetivo",
+    descricao: "A Cósmica Upcycle transforma peças antigas em novos...",
   },
 ];
 // função para mostrar os cards na tela
@@ -140,22 +140,22 @@ prevButton.addEventListener("click", () => {
 // Botão descrição dos produtos
 const maisCards = [
   {
-    text: "Descrição breve do produto eco-friendly... A minibag é a bolsa ideal para quem gosta de sair com pouca coisa. Com tamanho ideal para carregar apenas o necessário, ela é leve, discreta e confortável.Produzida de forma artesanal e afetuosa, com resíduo têxtil. Roupas descartadas e retalhos que receberam uma nova chance. Tem forro impermeável, alças acetinadas com 30mm e mosquetão de nylon ou cromado",
+    text: "A linha de bolsas da Cósmica Upcycle é um verdadeiro exemplo de sustentabilidade e inovação. Na imagem, vemos quatro bolsas dispostas em uma estrutura de metal, que simbolicamente remete a um lixo. No entanto, essa colocação não é um sinal de descaso, mas sim uma poderosa mensagem sobre a origem desses produtos. Cada uma dessas bolsas foi confeccionada a partir de tecidos velhos que, de outra forma, seriam descartados. A CósmiCa Upcycle transforma o que seria lixo em peças únicas e estilosas, promovendo a reutilização de materiais e a redução de resíduos. Essa abordagem não só contribui para a preservação do meio ambiente, mas também oferece aos consumidores produtos com uma história e um propósito. Escolher uma bolsa da CósmiCa Upcycle é optar por estilo consciente e sustentabilidade, mostrando que o luxo pode, sim, nascer do descarte.",
   },
   {
-    text: "Descrição breve do produto eco-friendly... Mochila ideal para quem tem um dia a dia um pouco mais cheio, ou gosta de estar sempre viajando. Ela possui diversos bolsos externos e um interno para notebook, com o limite de tela até 17,3. Um forro impermeável fácil de limpar. Alças e costas acolchoadas e com tecido respirável. Produzida de uma forma artesanal e afetuosa, com o resíduo têxtil. Roupas descartadas e retalhos que receberam uma nova chance.",
+    text: "Upeksha significa equanimidade. Mochila ideal para o dia-a-dia, possui diversos bolsos externos, bolso interno para notebook até 15,6\". Forro impermeável fácil de limpar. Alças e costas acolchoadas e com tecido respirável. Produzida de forma artesanal e afetuosa, com resíduo têxtil. Roupas descartadas e retalhos que receberam uma nova chance.",
   },
   {
-    text: "Descrição breve do produto eco-friendly... A minibag é a bolsa ideal para quem gosta de sair com pouca coisa. Com tamanho ideal para carregar apenas o necessário, ela é leve, discreta e confortável.Produzida de forma artesanal e afetuosa, com resíduo têxtil. Roupas descartadas e retalhos que receberam uma nova chance. Tem forro impermeável, alças acetinadas com 30mm e mosquetão de nylon ou cromado",
+    text: "Nossa primeira bolsa vendida aqui na Cósmica, há 4 anos ela é um sucesso entre nossos clientes. Leve e discreta seu tamanho parece pequeno, mas ela é uma bolsa muito espaçosa. Produzida de forma artesanal e afetuosa, com resíduo têxtil. Roupas descartadas e retalhos que receberam uma nova chance. Tem forro impermeável, alças acetinadas com 30mm, com mosquetões cromado. ",
   },
   {
-    text: "Descrição breve do produto eco-friendly... Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident numquam perspiciatis expedita tempore odit illo voluptatum, blanditiis eum? Quisquam provident obcaecati iure aliquid. Consequatur debitis dolorum quae sapiente corporis numquam!",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident numquam perspiciatis expedita tempore odit illo voluptatum, blanditiis eum? Quisquam provident obcaecati iure aliquid. Consequatur debitis dolorum quae sapiente corporis numquam!",
   },
   {
-    text: "Descrição breve do produto eco-friendly... A pochete Luna é a bolsa ideal para quem gosta de sair com pouca coisa. Com o tamanho ideal para carregar apenas o necessário, ela é leve, discreta e confortável. Produzida de forma artesanal e afetuosa. Com resíduo têxtil. Roupas descartadas e retalhos que receberam uma nova chance. Tem forro impermeável, alças acetinadas com 30mm, ela é colorida e contém um engate de nylon Ao comprar essa peça, você está incetivando uma economia mais justa e sustentável para o planeta.",
+    text: "A pochete Luna é a bolsa ideal para quem gosta de sair com pouca coisa. Com o tamanho ideal para carregar apenas o necessário, ela é leve, discreta e confortável. Produzida de forma artesanal e afetuosa. Com resíduo têxtil. Roupas descartadas e retalhos que receberam uma nova chance. Tem forro impermeável, alças acetinadas com 30mm, ela é colorida e contém um engate de nylon Ao comprar essa peça, você está incetivando uma economia mais justa e sustentável para o planeta.",
   },
   {
-    text: "Descrição breve do produto eco-friendly... A minibag é a bolsa ideal para quem gosta de sair com pouca coisa. Com tamanho ideal para carregar apenas o necessário, ela é leve, discreta e confortável.Produzida de forma artesanal e afetuosa, com resíduo têxtil. Roupas descartadas e retalhos que receberam uma nova chance. Tem forro impermeável, alças acetinadas com 30mm e mosquetão de nylon ou cromado",
+    text: "A Cósmica Upcycle transforma peças antigas em novos produtos carregados de memória e significado. Ao preferir descartar itens de valor sentimental, como uma calça de uma avó, a empresa resgata esses materiais e os transforma em mochilas, bolsas e outros acessórios. Cada peça passa por um processo cuidadoso, onde o valor emocional é preservado, mas também ganha nova vida e funcionalidade. A Cósmica Upcycle valoriza a história por trás de cada material, criando produtos que não são apenas sustentáveis, mas também repletos de afeto. É uma forma de honrar o passado enquanto se constrói um futuro mais consciente e cheio de significado.",
   },
 ];
 const produtoButton = document.querySelectorAll(".saiba-mais");
@@ -200,16 +200,16 @@ const nossaCausa = document.querySelectorAll(".icon"); // Altere o seletor confo
 
 const itens = ["design", "make", "package", "use", "recycle"];
 const texts = [
-  { id: 1, title: "Texto 1", text: "Lorem ipsum 1", centerText: "Center 1" },
-  { id: 2, title: "Texto 2", text: "Lorem ipsum 2" },
-  { id: 3, title: "Texto 3", text: "Lorem ipsum 3", centerText: "Center 2" },
-  { id: 4, title: "Texto 4", text: "Lorem ipsum 4" },
-  { id: 5, title: "Texto 5", text: "Lorem ipsum 5", centerText: "Center 3" },
-  { id: 6, title: "Texto 6", text: "Lorem ipsum 6" },
-  { id: 7, title: "Texto 7", text: "Lorem ipsum 7", centerText: "Center 4" },
-  { id: 8, title: "Texto 8", text: "Lorem ipsum 8" },
-  { id: 9, title: "Texto 9", text: "Lorem ipsum 9", centerText: "Center 5" },
-  { id: 10, title: "Texto 10", text: "Lorem ipsum 10" },
+  { id: 1, title: "Criatividade e Sustentabilidade em Transformação", text: "O design da moda upcycle combina sustentabilidade, criatividade e inovação para transformar peças de roupas e materiais descartados em novos produtos únicos e estilizados. Essa abordagem se baseia na reinvenção, utilizando tecidos antigos, sobras de produção ou roupas com defeitos para criar algo novo e funcional, sem a necessidade de produzir novas matérias-primas.", centerText: "O Design da moda upcycle" },
+  { id: 2, title: "Estilo Único com Impacto Sustentável", text: "Além de sua estética inovadora, o design da moda upcycle promove um impacto positivo ao reduzir o desperdício têxtil e minimizar os recursos naturais utilizados na produção. Ele também valoriza a individualidade do consumidor, oferecendo produtos que contam histórias e celebram a personalização. Dessa forma, o design upcycle não só redefine o que é considerado moda, mas também incentiva um consumo mais consciente e responsável." },
+  { id: 3, title: "O Processo Criativo e Sustentável da Moda Upcycle", text: "Processo de desenvolvimento da moda UpcycleO processo de desenvolvimento da moda upcycle envolve várias etapas criativas e sustentáveis que transformam peças de roupas ou materiais descartados em novos itens de valor. O primeiro passo é a seleção de materiais, onde roupas antigas, sobras de tecidos ou itens com defeitos são coletados. Esses materiais são então avaliados para determinar seu potencial de reaproveitamento, considerando texturas, cores e condições.", centerText: "Um Passo Sustentável para Reduzir o Desperdício e Preservar Recursos" },
+  { id: 4, title: "Da Seleção à Criação Consciente", text: "Na segunda etapa, ocorre a desconstrução e preparação, onde os itens selecionados são desmontados e limpos. Em seguida, vem a fase de design e criação, na qual designers e artesãos utilizam técnicas como recorte, costura, tingimento e bordado para reinventar as peças, muitas vezes combinando elementos de diferentes itens para criar algo único. A última etapa é a finalização e acabamento, garantindo que os produtos sejam funcionais, confortáveis e esteticamente atrativos. Todo o processo é guiado por uma abordagem consciente, que valoriza a redução de desperdício e a inovação, criando peças que carregam tanto estilo quanto propósito." },
+  { id: 5, title: "Uma Resposta Sustentável aos Desafios da Indústria", text: "O método de chegada do upcycle refere-se ao modo como essa prática sustentável ganhou espaço e reconhecimento no design, na moda e em outras indústrias. Esse conceito surgiu em resposta a problemas crescentes como o desperdício excessivo, a exploração de recursos naturais e os impactos negativos da produção em massa, especialmente na indústria têxtil.", centerText: "A chegada da moda Upcycle" },
+  { id: 6, title: "A Evolução do Upcycle: De Pequenos Artesãos ao Mercado Global", text: "Adotado por pequenos designers e artesões, o método de upcycle gradualmente conquistou espaço em grandes marcas e no mercado global, refletindo uma mudança nos valores dos consumidores. Hoje, ele é visto como uma alternativa inovadora que combina responsabilidade ambiental com estética, tornando-se uma parte essencial das estratégias de design sustentável." },
+  { id: 7, title: "Transformação Criativa com Propósito Sustentável", text: "A reciclagem na moda upcycle é uma prática que vai além do simples reaproveitamento de materiais. Diferente da reciclagem tradicional, onde os materiais são processados para serem reutilizados em novas formas, o upcycling preserva a integridade do material original, transformando roupas e tecidos descartados em peças de maior valor sem a necessidade de desmantelar ou processar quimicamente os materiais.", centerText: "A Reciclagem da Moda Upcycle" },
+  { id: 8, title: "Sustentabilidade com Menos Energia e Impacto Ambiental", text: "Esse processo reduz significativamente o consumo de energia e a emissão de poluentes, já que evita etapas industriais complexas. Por exemplo, ao reaproveitar tecidos de roupas antigas, como jeans ou camisas, é possível criar novos designs sem a necessidade de produzir novos tecidos, o que economiza água e reduz o uso de pesticidas e produtos químicos." },
+  { id: 9, title: "Transformando Estilo em Impacto Ambiental Positivo", text: "O impacto do uso da moda upcycle é significativo tanto para o meio ambiente quanto para a sociedade. No aspecto ambiental, essa prática reduz o desperdício têxtil, desviando roupas e tecidos descartados de aterros sanitários. Além disso, diminui a necessidade de produção de novas matérias-primas, como algodão ou poliéster, que consomem grandes quantidades de água e energia e geram emissões de carbono.", centerText: "Uso da Moda Upcycle" },
+  { id: 10, title: "Um Passo Sustentável para Reduzir o Desperdício e Preservar Recursos", text: "O impacto do uso da moda upcycle é significativo tanto para o meio ambiente quanto para a sociedade. No aspecto ambiental, essa prática reduz o desperdício têxtil, desviando roupas e tecidos descartados de aterros sanitários. Além disso, diminui a necessidade de produção de novas matérias-primas, como algodão ou poliéster, que consomem grandes quantidades de água e energia e geram emissões de carbono." },
 ];
 
 const itemToTextIds = {
@@ -264,7 +264,7 @@ function updateTexts(textIds) {
   // Reativa os textos com efeito após pequeno atraso
   setTimeout(() => {
     activateTexts();
-  }, 300); // Pequeno atraso para garantir a transição
+  }, 100); // Pequeno atraso para garantir a transição
 }
 
 // Configuração inicial: exibe o texto padrão (ID 1 e 2)
