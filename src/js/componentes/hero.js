58905ab17196245ba5896e4d9798f9c5e9301be1
Caio Morelli <caiomorelli.it@gmail.com>
2024-11-28 10:16:59 -0300

    Teste de carregamento de pag

--- src/js/componentes/hero.js
+++ src/js/componentes/hero.js
@@ -1,7 +1,8 @@
 function elementHero (){
     return `<div class="source-div"></div>
       <div id="svg-container"></div>
-      <p class="slogan">Quando você muda. O mundo muda</p>`
+      `
+      // <p class="slogan">Quando você muda. O mundo muda</p>
 }
 
 function svgHero (){
